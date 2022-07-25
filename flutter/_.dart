a8e7e06cfb8aaf1940d9dd3017ecd262d58d564e
elsayeddev <a.elsayed991999@gmail.com>
2022-07-25 14:28:52 +0200

    --

--- flutter/_.dart
+++ flutter/_.dart
@@ -4,8 +4,37 @@ mixin SingleTickerProviderStateMixin<T extends StatefulWidget> on State<T> {}
 
 class Widget {}
 
+class BuildContext {}
+
 class StatefulWidget extends Widget {}
 
 class StatelessWidget extends Widget {}
 
-class State<T extends Widget> {}
+abstract class State<T extends Widget> {
+  void initState() {}
+
+  Widget build(BuildContext context);
+
+  void didChangeDependencies() {}
+}
+
+class B extends StatelessWidget {}
+
+class AB extends StatefulWidget {}
+
+class A extends State<AB> with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Widget();
+  }
+}
