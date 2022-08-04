73059b864259e50f5502e9db20db6677c82913c1
elsayeddev <a.elsayed991999@gmail.com>
2022-08-04 19:34:39 +0200

    --

--- flutter/_.dart
+++ flutter/_.dart
@@ -45,14 +45,100 @@ class A extends State<AB> with SingleTickerProviderStateMixin {
   }
 }
 
-void main(List<String> args) {
-  // Array<Int8> arr = Array<Int8>.multi([
-  //   5,
-  //   6,
-  //   9,
-  //   1,
-  //   2,
-  // ]);
+class Node<T> {
+  Node({required this.value, this.next});
+  final T value;
+  Node<T>? next;
 
-  var arr = [5, 6, 6, 6, 9, 6, 6];
+  @override
+  String toString() => 'Node($value)';
+}
+
+class LinkedList<E> {
+  Node<E>? head;
+  Node<E>? tail;
+
+  bool get isEmpty => head == null;
+
+  void push(E value) {
+    head = Node(value: value, next: head);
+    tail ??= head;
+  }
+
+  void append(E value) {
+    if (isEmpty) {
+      push(value);
+    } else {
+      tail!.next = Node(value: value, next: null);
+      tail = tail!.next;
+    }
+  }
+
+  Node<E>? nodeAt(int index) {
+    var currentNode = head;
+    var currentIndex = 0;
+
+    while (currentNode != null && currentIndex < index) {
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+
+    return currentNode;
+  }
+
+  void insertAfter(Node<E> node, E value) {
+    if (tail == node) {
+      append(value);
+    }
+
+    node.next = Node(value: value, next: node.next);
+  }
+
+  void pop() {
+    /* 
+
+  
+  [value, NEXT1] => [value, NEXT2] => [value, NEXT3] => [value, NEXT4] => null 
+
+                    ^head                                        
+                                                        ^tail            
+
+    */
+    head = head?.next;
+
+    if (isEmpty) {
+      tail = null;
+    }
+  }
+
+  removeLast() {
+    if (head?.next == null) {
+      return pop();
+    }
+  }
+
+  @override
+  String toString() {
+    if (isEmpty) return 'LinkedList()';
+    var buffer = StringBuffer('LinkedList(');
+    var node = head;
+    while (node != null) {
+      buffer.write('${node.value}, ');
+      node = node.next;
+    }
+    buffer.write(')');
+    return buffer.toString();
+  }
+}
+
+void main(List<String> args) {
+  var list = LinkedList<int>();
+  list.append(10);
+  list.append(20);
+  list.append(30);
+  var middleNode = list.nodeAt(1)!;
+
+  list.insertAfter(middleNode, 100000);
+
+  print(list.toString());
 }
