6e3367b46b56dd4912b0b5467d0d013eee0f4a5f
elsayeddev <a.elsayed991999@gmail.com>
2022-10-02 09:58:39 +0200

    --

--- flutter/desgin pattern/observer_pattern.dart
+++ flutter/desgin pattern/observer_pattern.dart
@@ -1,80 +1,177 @@
-// import 'dart:async';
+import 'dart:async';
+import 'dart:math';
 
-// import 'package:flutter/foundation.dart';
+abstract class StockTicker {
+  late final String title;
+  late final Timer stockTimer;
+  Stock? _stock;
+  final _subscribers = <StockSubscriber>[];
 
-// abstract class StockTicker {
-//   late final String title;
-//   late final Timer stockTimer;
+  void subscribe(StockSubscriber subscriber) => _subscribers.add(subscriber);
+  void unsubscribe(StockSubscriber subscriber) =>
+      _subscribers.removeWhere((s) => s.id == subscriber.id);
+  void notifySubscribers() {
+    for (final subscriber in _subscribers) {
+      subscriber.update(_stock!);
+    }
+  }
 
-//   @protected
-//   Stock? stock;
+  void setStock(StockTickerSymbol stockTickerSymbol, int min, int max) {
+    final lastStock = _stock;
+    final price = (Random().nextInt(max - min) + min) / 100;
+    final changeAmount = lastStock != null ? price - lastStock.price : 0.0;
 
-//   final _subscribers = <StockSubscriber>[];
-//   void subscribe(StockSubscriber subscriber) => _subscribers.add(subscriber);
+    _stock = Stock(
+      changeAmount: changeAmount.abs(),
+      changeDirection: changeAmount > 0
+          ? StockChangeDirection.growing
+          : StockChangeDirection.falling,
+      price: price,
+      symbol: stockTickerSymbol,
+    );
+  }
 
-//   void unsubscribe(StockSubscriber subscriber) =>
-//       _subscribers.removeWhere((s) => s.id == subscriber.id);
+  void stopTicker() {
+    stockTimer.cancel();
+  }
+}
 
-//   void notifySubscribers() {
-//     for (final subscriber in _subscribers) {
-//       subscriber.update(stock!);
-//     }
-//   }
+class GameStopStockTicker extends StockTicker {
+  GameStopStockTicker() {
+    title = StockTickerSymbol.GME.toShortString();
+    stockTimer = Timer.periodic(
+      const Duration(seconds: 2),
+      (_) {
+        setStock(StockTickerSymbol.GME, 16000, 22000);
+        notifySubscribers();
+      },
+    );
+  }
+}
 
-//   void setStock(StockTickerSymbol stockTickerSymbol, int min, int max) {
-//     final lastStock = stock;
-//     final price = 80 / 100;
-//     final changeAmount = lastStock != null ? price - lastStock.price : 0.0;
+class GoogleStockTicker extends StockTicker {
+  GoogleStockTicker() {
+    title = StockTickerSymbol.GOOGL.toShortString();
+    stockTimer = Timer.periodic(
+      const Duration(seconds: 5),
+      (_) {
+        setStock(StockTickerSymbol.GOOGL, 200000, 204000);
+        notifySubscribers();
+      },
+    );
+  }
+}
 
-//     stock = Stock(
-//       changeAmount: changeAmount.abs(),
-//       changeDirection: changeAmount > 0
-//           ? StockChangeDirection.growing
-//           : StockChangeDirection.falling,
-//       price: price,
-//       symbol: stockTickerSymbol,
-//     );
-//   }
+class TeslaStockTicker extends StockTicker {
+  TeslaStockTicker() {
+    title = StockTickerSymbol.TSLA.toShortString();
+    stockTimer = Timer.periodic(
+      const Duration(seconds: 3),
+      (_) {
+        setStock(StockTickerSymbol.TSLA, 60000, 65000);
+        notifySubscribers();
+      },
+    );
+  }
+}
 
-//   void stopTicker() {
-//     stockTimer.cancel();
-//   }
-// }
+class Stock {
+  final StockTickerSymbol symbol;
+  final StockChangeDirection changeDirection;
+  final double price;
+  final double changeAmount;
 
-// class Stock {
-//   final StockTickerSymbol symbol;
-//   final StockChangeDirection changeDirection;
-//   final double price;
-//   final double changeAmount;
+  const Stock({
+    required this.symbol,
+    required this.changeDirection,
+    required this.price,
+    required this.changeAmount,
+  });
+}
 
-//   const Stock({
-//     required this.symbol,
-//     required this.changeDirection,
-//     required this.price,
-//     required this.changeAmount,
-//   });
-// }
+enum StockTickerSymbol {
+  GME,
+  GOOGL,
+  TSLA,
+}
 
-// enum StockTickerSymbol {
-//   GME,
-//   GOOGL,
-//   TSLA,
-// }
+extension StockTickerSymbolExtension on StockTickerSymbol {
+  String toShortString() => toString().split('.').last;
+}
 
-// extension StockTickerSymbolExtension on StockTickerSymbol {
-//   String toShortString() => toString().split('.').last;
-// }
+enum StockChangeDirection {
+  falling,
+  growing,
+}
 
-// abstract class StockSubscriber {
-//   late final String title;
+abstract class StockSubscriber {
+  late final String title;
 
-//   final id = faker.guid.guid();
+  final id = Random().nextInt(50);
 
-//   @protected
-//   final StreamController<Stock> stockStreamController =
-//       StreamController.broadcast();
+  final StreamController<Stock> stockStreamController =
+      StreamController.broadcast();
 
-//   Stream<Stock> get stockStream => stockStreamController.stream;
+  Stream<Stock> get stockStream => stockStreamController.stream;
 
-//   void update(Stock stock);
-// }
+  // final StreamController<Stock> _stockStreamController =
+  //     StreamController.broadcast();
+
+  // Stream<Stock> get stockStream => _stockStreamController.stream;
+
+  void update(Stock stock);
+}
+
+class DefaultStockSubscriber extends StockSubscriber {
+  DefaultStockSubscriber() {
+    title = 'All stocks';
+  }
+
+  @override
+  void update(Stock stock) {
+    print('DefaultStockSubscriber: $stock');
+    stockStreamController.add(stock);
+  }
+}
+
+class GrowingStockSubscriber extends StockSubscriber {
+  GrowingStockSubscriber() {
+    title = 'Growing stocks';
+  }
+
+  @override
+  void update(Stock stock) {
+    if (stock.changeDirection == StockChangeDirection.growing) {
+      print('GrowingStockSubscriber: $stock');
+      stockStreamController.add(stock);
+    }
+  }
+}
+
+main() async {
+  // final _stockSubscriberList = <StockSubscriber>[
+  //   DefaultStockSubscriber(),
+  //   GrowingStockSubscriber(),
+  // ];
+
+  // final _stockEntries = <Stock>[];
+
+  StockSubscriber _subscriber = DefaultStockSubscriber();
+  // int _selectedSubscriberIndex = 0;
+
+  await _subscriber.stockStream.listen((stock) {
+    print("readd");
+  });
+  // void _onStockChange(Stock stock) {
+  //   _stockEntries.add(stock);
+  // }
+
+  // void _setSelectedSubscriberIndex(int? index) {
+  //   _stockStreamSubscription?.cancel();
+
+  //   _stockEntries.clear();
+  //   _selectedSubscriberIndex = index!;
+  //   _subscriber = _stockSubscriberList[_selectedSubscriberIndex];
+  //   _stockStreamSubscription = _subscriber.stockStream.listen(_onStockChange);
+  // }
+}
